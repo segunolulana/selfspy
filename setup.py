3681d2a772b74b3499ecf55f1bc8c183084ccd9f
Gianluca Segato <gianluca.segato@gmail.com>
2019-03-02 11:42:52 +0100

    Added ./idea files to .gitignore. Improved setup installation: - updated packages requirements (fixes compatibility issues) - refactored setup.py code - ported setup.py to Python3

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 import os
 import platform
+from setuptools import setup
 
-# dep_link = []
+
 if platform.system() == 'Darwin':
-    req_file = 'osx-requirements.txt'
+    requisites = 'osx-requirements.txt'
+    # needed for installing pycrypto
+    os.environ["CFLAGS"] = "-I/usr/local/include -L/usr/local/lib"
 elif platform.system() == "Windows":
-    req_file = "win-requirements.txt"
+    requisites = "win-requirements.txt"
 else:
-    req_file = 'requirements.txt'
-    #dep_link = ['http://python-xlib.svn.sourceforge.net/viewvc/python-xlib/tags/xlib_0_15rc1/?view=tar#egg=pyxlib']
+    requisites = 'requirements.txt'
 
-with open(os.path.join(os.path.dirname(__file__), req_file)) as f:
+with open(os.path.join(os.path.dirname(__file__), requisites)) as f:
     requires = list(f.readlines())
 
-print '"%s"' % requires
-
-from setuptools import setup
+print('"%s"' % requires)
 
 setup(name="selfspy",
       version='0.3.0',
       packages=['selfspy'],
       author="David Fendrich",
-      # author_email='',
       description=''.join("""
           Log everything you do on the computer, for statistics,
           future reference and all-around fun!
       """.strip().split('\n')),
       install_requires=requires,
-      #dependency_links=dep_link,
       entry_points=dict(console_scripts=['selfspy=selfspy:main',
                                          'selfstats=selfspy.stats:main']))
